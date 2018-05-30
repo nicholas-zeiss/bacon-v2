bc864ffb0acd4cc100b2cdb61130b317fe3a7bd5
nicholas-zeiss <nicholas.zeiss@gmail.com>
2018-05-29 20:41:17 -0400

    reconfigured server for production

--- server/server.js
+++ server/server.js
@@ -16,16 +16,11 @@ const app = express();
 
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../../app')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../src/index.html'));
-});
-
-
-app.get('/smoothscroll', (req, res) => {
-	res.sendFile('../src/assets/smoothscroll.js');
+	res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 
